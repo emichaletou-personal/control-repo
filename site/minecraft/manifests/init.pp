cf858303805e613ac1189f5b7a1549756401e7cf
Eleni Michaletou <59175267+emichaletou-personal@users.noreply.github.com>
2019-12-25 23:05:27 +0200

    commit

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -25,7 +25,7 @@ class minecraft (
   file { '/etc/systemd/system/minecraft.service':
     ensure  => file,
     content  => epp('minecraft/minecraft.service',{
-      install_dir = $install_dir,
+      install_dir => $install_dir,
     })
   }
 
