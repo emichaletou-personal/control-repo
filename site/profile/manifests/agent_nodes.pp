5e55c24f2560f20e81b711264b460f74ad6e7cc3
Eleni Michaletou <59175267+emichaletou-personal@users.noreply.github.com>
2019-12-28 19:49:23 +0200

    commit

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,17 +1,17 @@
 class profile::agent_nodes {
   
-  include dockeragent 
+  include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
   dockeragent::node {'minetest.puppet.vm':}
 
   host {'web.puppet.vm':
-    ensure => present, 
-    ip     => '172.18.0.3', 
+    ensure => present,
+    ip     => '172.18.0.3',
   }
 
   host {'db.puppet.vm':
-    ensure => present, 
-    ip     => '172.18.0.2', 
+    ensure => present,
+    ip     => '172.18.0.2',
   }
 }
